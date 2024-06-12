441fa3652d6f3357678e446b349e033434408e86
Lucasdeekay <lucasdeekay98@gmail.com>
2024-06-12 19:03:26 +0100

    Updated project

--- StudentMarketplaceAPI/settings.py
+++ StudentMarketplaceAPI/settings.py
@@ -89,10 +89,10 @@ SECRET_KEY = config('SECRET_KEY')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
+        'NAME': config('POSTGRES_DATABASE'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='5432'),
     }
 }
